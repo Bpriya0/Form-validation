7206073809108485577df08bb8a14a0229362659
Bishnupriya Pradhan <86057106+Bpriya0@users.noreply.github.com>
2021-09-05 04:09:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import { Switch, Route } from "react-router-dom";
+import FormSuccess from './components/FormSuccess';
+import LoginSuccess from './components/LoginSuccess';
+import Login  from './components/Login';
+import List from './components/List';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Form />
+    <Switch>
+       
+        <Route exact path="/form" component={Form} />
+        <Route exact path="/form/success" component={FormSuccess} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/login/success" component={LoginSuccess} />
+        <Route exact path="/List" component={List} />
+      </Switch>
+  
+  </>
+  )
 }
 
 export default App;
